28c88f4aef1a7ad393c5066d44619909aed03904
Hugo-lecloerec <hugo.lecloerec@gmail.com>
2022-01-20 15:18:43 +0100

    First draft header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,13 @@
 // == Import
-import reactLogo from './react-logo.svg';
+
+import NavBar from 'src/components/NavBar';
+
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <NavBar />
   </div>
 );
 
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import logo from './logo.jpg';
+import './NavBar.scss';
+
+const NavBar = () => (
+  <div className="menu">
+    <div className="menu--logo">
+      <img src={logo} alt="logo des culs fouettés" />
+    </div>
+    <div>
+      <li>
+        <Link to="/" className="menu-link">Accueil</Link>
+      </li>
+      <li>
+        <Link to="/nos-produits" className="menu-link">Nos produits</Link>
+      </li>
+      <li>
+        <Link to="/a-propos" className="menu-link">A propos</Link>
+      </li>
+      <li>
+        <Link to="/mon-compte" className="menu-link">Mon compte</Link>
+      </li>
+    </div>
+    <input type="text" placeholder="Search.." className="menu--searchbar" />
+    <div className="menu--user">
+      <li>
+        <Link to="/connexion" className="menu-link">Se connecter</Link>
+      </li>
+      <li>
+        <Link to="/panier" className="menu-link">Mon panier</Link>
+      </li>
+    </div>
+  </div>
+);
+export default NavBar;
