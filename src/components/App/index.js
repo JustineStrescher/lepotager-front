f20fa9f0557e56115bec822a883c704092fefff1
Hugo-lecloerec <hugo.lecloerec@gmail.com>
2022-01-24 11:10:09 +0100

    Merging files

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,22 @@
 // == Import
-
 import NavBar from 'src/components/NavBar';
+import Home from 'src/components/Home';
+import About from 'src/components/About';
+import Products from 'src/components/Products';
+import Account from 'src/components/Account';
+import './styles.css';
+import { Route, Routes } from 'react-router-dom';
 
-import './styles.scss';
-
-// import Footer from '../Footer';
-
-// == Composant
+// == Composants
 const App = () => (
   <div className="app">
     <NavBar />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/nos-produits" element={<Products />} />
+      <Route exact path="/a-propos" element={<About />} />
+      <Route exact path="/se-connecter" element={<Account />} />
+    </Routes>
   </div>
 );
 
