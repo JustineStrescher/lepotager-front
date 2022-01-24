4b1e887fd65e9fd5ad3af57707c0237d3deadc07
clementllc <clement.le.lec5@gmail.com>
2022-01-24 22:00:31 +0100

    fix footer, filter  product and title for products on familly's page

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,18 +1,33 @@
 import './Product.scss';
 
+import { useParams } from 'react-router';
 import { useSelector } from 'react-redux';
+
+import { findVegetableByFamilly, findCategory } from '../../selectors/vegetables';
 
 import ProductCard from './ProductCard';
 
 const Product = () => {
-  const productList = useSelector((state) => state.product.ProductData);
+  const parameters = useParams();
+  const currentSlug = parameters.slug;
+  // ici on utilise useSelector pour récuperé la recette actuelle en fonction de la liste
+  // des recettes présentes dans le state
+  const productList = (
+    useSelector((state) => findVegetableByFamilly(state.product.ProductData, currentSlug)));
+
+  const category = useSelector((state) => findCategory(state.product.ProductData, currentSlug));
 
   return (
-    <div className="Product">
-      {productList.map((product) => (
-        <ProductCard key={product.id} {...product} />
-      ))}
-    </div>
+    <section>
+      <div className="Product--title">
+        <p>{category}</p>
+      </div>
+      <div className="Product">
+        {productList.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </div>
+    </section>
   );
 };
 
