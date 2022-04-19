6875d7ef8a6ab7dfe0cb6977a5cf5b7db9ce4325
JustineStrescher <justine.cioe@gmail.com>
2022-04-19 23:02:41 +0200

    misspell correction

--- src/components/AboutConcept/Concept/index.js
+++ src/components/AboutConcept/Concept/index.js
@@ -4,19 +4,19 @@ const Concept = () => (
   <div className="concept--content text">
     <div className="concept-flex margin-top">
       <div className="concept-numero title">1</div>
-      <p>Inscriver/connecter vous sur le site.</p>
+      <p>Inscrivez/connectez vous sur le site.</p>
     </div>
     <div className="concept-flex">
       <div className="concept-numero Main inverse1">2</div>
-      <p>Ajouter vos produits preferer au panier </p>
+      <p>Ajoutez vos produits préferer au panier </p>
     </div>
     <div className="concept-flex">
       <div className="concept-numero alt">3</div>
-      <p>Valider votre commande</p>
+      <p>Validez votre commande</p>
     </div>
     <div className="concept-flex">
       <div className="concept-numero dark inverse1">4</div>
-      <p>Venez recuperer votre panier au potager le mercredi .</p>
+      <p>Venez récuperer votre panier à notre domicile le mercredi .</p>
     </div>
   </div>
 );
